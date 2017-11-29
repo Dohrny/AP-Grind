f5e523760562ec60c6080592d7777e03e69fed9f
Adam <Dohrny@users.noreply.github.com>
2017-11-29 17:43:14 -0600

    adding timer from internet

--- script.js
+++ script.js
@@ -8,7 +8,9 @@ var resources = 0;
 var notifier;
 var APtext;
 var AKtext;
+var AKtimerText
 var championsText;
+var AKready
 var championsReady
 var dungeonReady
 
@@ -18,11 +20,12 @@ window.onload = function () {
     championsText = document.getElementById("champions");
     resourcesText = document.getElementById("resources")
     notifier = document.getElementById("notifications");
-    AKtext.innerHTML = AKlevel.toString();
-    championsText.innerHTML = champions.toString();
+    AKtimerText = document.getElementById("AKtimer")
+    updateValues()
     championsReady = true
     dungeonReady = true
     championCost = 1
+    AKready = true
 };
 //add AP every second
 window.setInterval(updateAP, 1000);
@@ -37,18 +40,36 @@ function addChampion() {
             champions += 1;
             resources -= championCost
             championCost = championCost * 10
-            championsText.innerHTML = champions.toString();
-            resourcesText.innerHTML = resources.toString()
+            updateValues()
             notifier.innerHTML = "added a Champion";
         }
     }
 }
 //increase AK by 1. --add cost and time gate
 function increaseAK() {
-    AKlevel += 1;
-    AKtext.innerHTML = AKlevel.toString();
-    AKpercent = Math.round(AKpercent * 1.3)
+    if (AKready) {
+        AKlevel += 1;
+        AKtext.innerHTML = AKlevel.toString();
+        AKpercent = Math.round(AKpercent * 1.3)
+        AKready = false
+        
+        var timer = new CountDownTimer(5)
+        var timeObj = CountDownTimer.parse(5)
+            
+        format(timeObj.minutes, timeObj.seconds)
+        timer.onTick(format)
+
+        timer.start()
+    }
+    function format(minutes, seconds) {
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+        AKtimerText.innerHTML = minutes + ':' + seconds;
+    }
 }
+
+
+
 //give reward for pressing button. has a chance to fail
 function runDungeon() {
     if (dungeonReady == true) {
@@ -107,15 +128,52 @@ function rewardPlayer() {
     if (reward == 0) {
         var APgained = Math.floor(AKpercent / 10);
         AP += APgained;
-        APtext.innerHTML = AP.toString();
+        updateValues()
         notifier.innerHTML = "gained " + APgained + "AP";
         console.log(AKpercent)
     }
     if (reward == 1) {
         resources += 10;
-        resourcesText.innerHTML = resources.toString();
+        updateValues()
         notifier.innerHTML = "gained 10 resources";
     }
 }
 
+function updateValues() {
+    APtext.innerHTML = AP.toLocaleString()
+    AKtext.innerHTML = AKlevel.toString()
+    resourcesText.innerHTML = resources.toString()
+    championsText.innerHTML = champions.toString()
+}
+
+
+/* function startTimer(duration, display) {
+    var start = Date.now(),
+        diff,
+        minutes,
+        seconds;
+    function timer() {
+        // get the number of seconds that have elapsed since 
+        // startTimer() was called
+        diff = duration - (((Date.now() - start) / 1000) | 0);
+
+        // does the same job as parseInt truncates the float
+        minutes = (diff / 60) | 0;
+        seconds = (diff % 60) | 0;
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds; 
+
+        if (diff <= 0) {
+            // add one second so that the count down starts at the full duration
+            // example 05:00 not 04:59
+            start = Date.now() + 1000;
+        }
+    };
+    // we don't want to wait a full second before the timer starts
+    timer();
+    setInterval(timer, 1000);
+} */
 
